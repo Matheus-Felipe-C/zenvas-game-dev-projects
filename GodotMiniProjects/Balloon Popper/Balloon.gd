93ed083d216fd406afec7579db4c472c090c9eb5
Matheus Felipe <theusf40@hotmail.com>
2024-04-28 10:31:05 -0300

    Finished Balloon Popper minigame

---
+++ GodotMiniProjects/Balloon Popper/Balloon.gd
@@ -0,0 +1,16 @@
+extends Area3D
+
+@export var clicks_top_pop: int = 3
+@export var size_increase: float = 0.2
+@export var score_to_give: int = 1
+
+
+
+func _on_input_event(camera:Node, event:InputEvent, position:Vector3, normal:Vector3, shape_idx:int):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		scale += Vector3.ONE * size_increase
+		clicks_top_pop -= 1
+
+		if clicks_top_pop == 0:
+			get_node("/root/Main").increase_score(score_to_give)
+			queue_free()
